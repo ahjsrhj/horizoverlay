be47ae21d102938ae17f6f2a9000d4935d28af98
ahjsrhj <hexrhj@gmail.com>
2020-04-20 21:14:22 +0800

    调整发布脚本

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import Config from './Config'
 import NotFound from './NotFound'
 import SetupMode from './SetupMode'
 import initActWebSocket from './actwebsocket'
-require('./testing/testing.js')
 
 // import Raven from 'raven-js'
 // import { sentryUrl } from './sentry'
