0446ce3d2be07aceeb54f8875288ea6429ab71bf
ahjsrhj <ahjsrhj@users.noreply.github.com>
2020-10-12 06:35:58 +0000

    deploy: 2c8166bf4627d9c8d9d34b7a0f4357eb8884324b

---
+++ precache-manifest.9a013fb4212face2b8bc241a86f1a1c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496528b08cd168062197395909e7f3e1",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/index.html"
+  },
+  {
+    "revision": "023395f1d9b638c561b1",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/static/css/main.4fac3df4.chunk.css"
+  },
+  {
+    "revision": "908e049af65e8916b782",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/static/js/2.a9db8167.chunk.js"
+  },
+  {
+    "revision": "023395f1d9b638c561b1",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/static/js/main.c7640816.chunk.js"
+  },
+  {
+    "revision": "a83d0b470c27a4f9f2f1",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/static/js/runtime~main.490adace.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/static/media/mog.e1e18820.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dbfa429b6e9fd69d9a6e904630274533.js"
+  "https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/precache-manifest.9a013fb4212face2b8bc241a86f1a1c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/ahjsrhj/horizoverlay@gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
