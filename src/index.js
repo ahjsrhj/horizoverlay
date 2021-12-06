91e192a02a90e9bf947bd8fe1d7459d12cef7fe8
renhongjian <renhongjian@accesscorporate.com.cn>
2021-12-06 17:11:24 +0800

    feat(监听): 修改数据入口 添加动画自动隐藏

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
+/** @format */
+
 // uncomment for testing
 // import './testing/testing'
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import OverlayAPI from 'ffxiv-overlay-api';
+import React from "react";
+import ReactDOM from "react-dom";
+import OverlayAPI from "ffxiv-overlay-api";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Overlay from './Overlay'
-import Config from './Config'
-import NotFound from './NotFound'
-import SetupMode from './SetupMode'
-import initActWebSocket from './actwebsocket'
+import Overlay from "./Overlay";
+import Config from "./Config";
+import NotFound from "./NotFound";
+import SetupMode from "./SetupMode";
+// import initActWebSocket from "./actwebsocket";
 
 // require('./testing/testing.js')
 
-require(`./images/handle.png`)
+require(`./images/handle.png`);
 
-initActWebSocket()
+// initActWebSocket();
 
-window.lastData = {}
-const Inactive = detail => {
+window.lastData = {};
+const Inactive = (detail) => {
   return (
     <Router basename={`${process.env.PUBLIC_URL}`}>
       <Switch>
@@ -28,10 +30,17 @@ const Inactive = detail => {
         <Route component={SetupMode} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-const Root = detail => {
+let inCutscene = false;
+let primaryID = null;
+
+const InCutscene = () => {
+  return null;
+};
+
+const Root = (detail) => {
   return (
     <Router basename={`${process.env.PUBLIC_URL}`}>
       <Switch>
@@ -40,68 +49,62 @@ const Root = detail => {
         <Route render={() => <NotFound text="Page Not Found!" />} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-// This will run when data is ON
-function onOverlayDataUpdate(e) {
-  const detail = (e.detail.msg ? e.detail.msg : e.detail)
-  
-  ReactDOM.render(<Root {...detail} />, document.getElementById('root'))
-}
-// This will run when there's no data
-ReactDOM.render(<Inactive />, document.getElementById('root'))
+let lastDetail = null;
 
-// :: Events
-// https://github.com/RainbowMage/OverlayPlugin/wiki/JavaScript-API-reference
-// https://github.com/hibiyasleep/OverlayPlugin/wiki/Additional-Javascript-API-Reference
-
-// - onOverlayDataUpdate
-// This event occurs when the OverlayPlugin sends the new data.
-document.addEventListener('onOverlayDataUpdate', onOverlayDataUpdate)
-
-// - onLogLine
-// This event occurs when anything in the chat happens, so we need to clean it up a bit before sending to the component or else it will polute and re-render it a lot unnecessarily
-// Not being implemented right now
-// document.addEventListener('onLogLine', onOverlayDataUpdate)
-
-// - onOverlayStateUpdate
-// This event occurs when the overlay setting has changed.
-document.addEventListener('onOverlayStateUpdate', function(e) {
-  if (!e.detail.isLocked) {
-    document.documentElement.classList.add('resizable')
+function renderDetail(detail) {
+  if (!detail) {
+    ReactDOM.render(<Inactive />, document.getElementById("root"));
   } else {
-    document.documentElement.classList.remove('resizable')
+    lastDetail = detail;
+    ReactDOM.render(<Root {...detail} />, document.getElementById("root"));
   }
-})
-// Receiver of OverlayPluginApi.sendMessage and OverlayPluginApi.broadcastMessage, not being used as far as I know
-window.addEventListener('message', function(e) {
-  if (e.data.type === 'onOverlayDataUpdate') {
-    onOverlayDataUpdate(e.data)
-  }
-})
+}
 
+function render(detail) {
+  if (inCutscene) {
+    // 过场中
+    if (detail) {
+      if (detail.isActive === "false") {
+        // 战斗结束隐藏
+        ReactDOM.render(<InCutscene />, document.getElementById("root"));
+      } else {
+        renderDetail(detail);
+      }
+    } else {
+      ReactDOM.render(<InCutscene />, document.getElementById("root"));
+    }
+  } else {
+    // 过场结束
+    renderDetail(detail);
+  }
+}
+render();
 
 // 新的监听方式
 // OverlayAPI.
 const overlay = new OverlayAPI({
   extendData: true,
-  silentMode: false,
+  silentMode: true,
 });
 
-overlay.addListener('CombatData', (data) => {
-  console.log('listener of `CombatData`', data);
+overlay.addListener("CombatData", (data) => {
+  render(data);
 });
-overlay.addListener('ChangeZone', (data) => {
-  console.log('listener of `ChangeZone`', data);
-});
-overlay.addListener('OnlineStatusChanged', (data) =>{
-  console.log('listener of `OnlineStatusChanged`', data);
-  if (data.status === 'InCutscene') {
-    // 过场动画中
-  } else {
-    // 离开过场动画
+// overlay.addListener('ChangeZone', (data) => {
+//   console.log('listener of `ChangeZone`', data);
+// });
+overlay.addListener("OnlineStatusChanged", (data) => {
+  if (primaryID === data.target) {
+    // console.log("listener of `OnlineStatusChanged`", data);
+    inCutscene = data.status === "InCutscene";
+    render(inCutscene ? null : lastDetail);
   }
-})
+});
+overlay.addListener("ChangePrimaryPlayer", (data) => {
+  primaryID = data.charID;
+});
 
 overlay.startEvent();
